4622aadc859c7cebe88de0e825b6474a1eb02ba6
PawelKrawczyk96 <pawel.krawczyk@interia.com>
2022-04-01 11:53:23 +0200

    stash

---
+++ pages/single-offer/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from 'next'
+
+import { StoreDataAdapter } from '../../components/storeDataAdapter'
+import { LayoutTop } from '../../components/layout/layoutTop'
+import Header from '../../components/singleOffer/Header/Header'
+import Offer from '../../components/singleOffer/Offer/Offer'
+import { observer } from 'mobx-react'
+import { StoreContext } from '../_app'
+import { useContext } from 'react'
+
+const MyOffers: NextPage = observer(() => {
+  const store = useContext(StoreContext)
+
+  const [data, setData] = store.Offers
+  return (
+    <StoreDataAdapter>
+      {console.log(data)}
+      <div className='page my-offers'>
+        <LayoutTop />
+        <Header
+          collectionName='test'
+          nftAddress='4G7tnw4nFy1zK8KW77HJyDn4tTU4vFcwQJF3FF9ek5Rj'
+          nftImage='test'
+          nftName='test'
+          website='unloc.xyz'
+        />
+        <div className='offer-grid'>
+          <Offer
+            offerID='1234-1234-1234-1234'
+            status='Active'
+            amount='10000'
+            token='USDC'
+            duration='90'
+            durationRemaning='20'
+            APR='20'
+            totalReapy='10293'
+          />
+        </div>
+      </div>
+      <div className='home-bg-bottom' />
+    </StoreDataAdapter>
+  )
+})
+
+export default MyOffers
