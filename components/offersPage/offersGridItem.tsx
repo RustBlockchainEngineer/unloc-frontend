52be00b81b4a10d797cf3ca9b561ba385d56b7e3
Dawid Gorczyca <kaiga747@gmail.com>
2022-04-01 14:13:18 +0200

    offers grid

--- components/offersPage/offersGridItem.tsx
+++ components/offersPage/offersGridItem.tsx
@@ -5,14 +5,33 @@ import Link from 'next/link'
 interface OffersGridItemInterface {
   subOfferKey: string
   image: string
+  apr: number
+  amount: number
+  duration: number
+  currency: string
 }
 
-export const OffersGridItem = ({ subOfferKey, image }: OffersGridItemInterface) => {
+export const OffersGridItem = ({ subOfferKey, image, apr, amount, duration, currency }: OffersGridItemInterface) => {
   return (
     <div className='offers-grid-item' key={subOfferKey}>
       <Link href={`/offers/${subOfferKey}`}>
         <a>
-          <div className='hover-data'></div>
+          <div className='hover-data'>
+            <div className='hover-data-item'>
+              <span className='label'>APR</span>
+              <span className='content'>{apr} %</span>
+            </div>
+            <div className='hover-data-item'>
+              <span className='label'>Amount</span>
+              <span className='content'>
+                {amount} {currency}
+              </span>
+            </div>
+            <div className='hover-data-item'>
+              <span className='label'>Duration</span>
+              <span className='content'>{duration} Days</span>
+            </div>
+          </div>
           {image ? <Image src={image} alt='NFT Picture' width={500} height={500} /> : ''}
         </a>
       </Link>
