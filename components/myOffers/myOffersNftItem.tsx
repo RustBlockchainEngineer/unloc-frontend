8371608326c40c9351d356d99db2766347615696
PawelKrawczyk96 <pawel.krawczyk@interia.com>
2022-04-06 18:15:59 +0200

    add ligboxloan

--- components/myOffers/myOffersNftItem.tsx
+++ components/myOffers/myOffersNftItem.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { compressAddress } from '../../utils/stringUtils/compressAdress'
 import arrowImg from '../../constants/icons/png/Vector.png'
 import icons from '../../constants/icons/icons'
 import Image from 'next/image'
 
 import { MyOffersNftOffer } from './myOffersNftOffers'
+import { StoreContext } from '../../pages/_app'
+import { Lightbox } from '../lightboxes/lightbox'
+import CreateLoan from '../lightboxes/createLoan/createLoan'
+import { observer } from 'mobx-react'
 interface MyOffersNftItemProps {
   offerKey: string
   nftMint: string
@@ -24,112 +28,123 @@ const setNFTState = (status: number) => {
   if (status === 1) return <p style={{ color: 'green' }}>Accepted</p>
 }
 
-export const MyOffersNftItem: React.FC<MyOffersNftItemProps> = ({
-  nftMint,
-  name,
-  image,
-  offers,
-  state,
-  classNames,
-  reveal,
-  offerKey,
-  onReveal,
-  handleCreateSubOffer,
-  handleRepayLoan
-}) => {
-  const setNFTActions = (status: number) => {
-    if (status === 0) {
-      return (
-        <div className='nft-info-buttons'>
-          <button className=' btn--md btn--primary' onClick={() => handleCreateSubOffer(nftMint)}>
-            Create Offer
-          </button>
-        </div>
-      )
+export const MyOffersNftItem: React.FC<MyOffersNftItemProps> = observer(
+  ({
+    nftMint,
+    name,
+    image,
+    offers,
+    state,
+    classNames,
+    reveal,
+    offerKey,
+    onReveal,
+    handleCreateSubOffer,
+    handleRepayLoan
+  }) => {
+    const store = useContext(StoreContext)
+    const { showLightboxLoan, setShowLightboxLoan } = store.Lightbox
+
+    const setNFTActions = (status: number) => {
+      if (status === 0) {
+        return (
+          <div className='nft-info-buttons'>
+            <button className=' btn--md btn--primary' onClick={() => store.Lightbox.setShowLightboxLoan('create')}>
+              Create Offer
+            </button>
+          </div>
+        )
+      }
+      if (status === 1) {
+        return (
+          <div className='nft-info-buttons'>
+            <button className=' btn--md btn--disabled'>NFT Locked, Loan Offer Taken</button>
+          </div>
+        )
+      }
     }
-    if (status === 1) {
-      return (
-        <div className='nft-info-buttons'>
-          <button className=' btn--md btn--disabled'>NFT Locked, Loan Offer Taken</button>
-        </div>
-      )
-    }
-  }
-  return (
-    <div className='nft-list-item'>
-      <div className={`my-offers-nft ${classNames ? classNames : ''}`}>
-        {name && image ? (
-          <div className='nft-item'>
-            <div className='nft-wrapper'>
-              <div className='nft-info'>
-                <Image src={image} alt='NFT Image' width='50px' height='50px' className='nft-img' />
-                <div className='nft-info-inner'>
-                  <div className='nft-info-inner-mainData'>
-                    <div className='info-description'>
-                      <p>Offer ID:</p>
-                      <p>{compressAddress(4, offerKey)}</p>
+    return (
+      <div className='nft-list-item'>
+        <div className={`my-offers-nft ${classNames ? classNames : ''}`}>
+          {name && image ? (
+            <div className='nft-item'>
+              <div className='nft-wrapper'>
+                <div className='nft-info'>
+                  <Image src={image} alt='NFT Image' width='50px' height='50px' className='nft-img' />
+                  <div className='nft-info-inner'>
+                    <div className='nft-info-inner-mainData'>
+                      <div className='info-description'>
+                        <p>Offer ID:</p>
+                        <p>{compressAddress(4, offerKey)}</p>
+                      </div>
+                      <Image
+                        src={icons.copy}
+                        width='18px'
+                        height='18px'
+                        className='clipboard-button'
+                        onClick={() => navigator.clipboard.writeText(offerKey)}
+                      />
                     </div>
-                    <Image
-                      src={icons.copy}
-                      width='18px'
-                      height='18px'
-                      className='clipboard-button'
-                      onClick={() => navigator.clipboard.writeText(offerKey)}
-                    />
+                    <p className='info-name'>{name}</p>
                   </div>
-                  <p className='info-name'>{name}</p>
+                </div>
+                {setNFTActions(state)}
+              </div>
+              <div className='nft-metadata'>
+                <div className='metadata-item'>
+                  <p>NFT mint</p>
+                  <p>{compressAddress(4, nftMint)}</p>
+                </div>
+                <div className='metadata-item'>
+                  <p>collection</p>
+                  <p></p>
+                </div>
+                <div className='metadata-item'>
+                  <p>status</p>
+                  {setNFTState(state)}
                 </div>
               </div>
-              {setNFTActions(state)}
             </div>
-            <div className='nft-metadata'>
-              <div className='metadata-item'>
-                <p>NFT mint</p>
-                <p>{compressAddress(4, nftMint)}</p>
-              </div>
-              <div className='metadata-item'>
-                <p>collection</p>
-                <p></p>
-              </div>
-              <div className='metadata-item'>
-                <p>status</p>
-                {setNFTState(state)}
-              </div>
-            </div>
-          </div>
-        ) : (
-          <>Loading NFT Data</>
-        )}
-      </div>
-      <div
-        className='offers-list'
-        style={{ maxHeight: `${reveal ? '1000px' : '22px'}`, backgroundColor: `${reveal ? '#482688' : ''}` }}
-        onClick={() => onReveal(offerKey)}
-      >
-        <div className='offers-reveal-btn'>
-          <p>{reveal ? 'Active Loan' : 'Offers for this NFT'}</p>
-          <Image src={icons.reveal} />
-        </div>
-        <div className='offers-items'>
-          {offers && offers.length ? (
-            offers.map((offer: any) => (
-              <MyOffersNftOffer
-                key={offer.subOfferKey.toBase58()}
-                offerAmount={offer.offerAmount}
-                APR={offer.aprNumerator}
-                status={offer.state}
-                offerID={offer.subOfferKey}
-                duration={offer.loanDuration}
-                repaid={offer.minRepaidNumerator}
-              />
-            ))
           ) : (
-            <div className={`my-offers-no__offers ${classNames ? classNames : ''}`}>
-              <p>You have no offers yet</p>
-            </div>
+            <>Loading NFT Data</>
           )}
         </div>
+        <div
+          className='offers-list'
+          style={{ maxHeight: `${reveal ? '1000px' : '22px'}`, backgroundColor: `${reveal ? '#482688' : ''}` }}
+          onClick={() => onReveal(offerKey)}
+        >
+          <div className='offers-reveal-btn'>
+            <p>{reveal ? 'Active Loan' : 'Offers for this NFT'}</p>
+            <Image src={icons.reveal} />
+          </div>
+          <div className='offers-items'>
+            {offers && offers.length ? (
+              offers.map((offer: any) => (
+                <MyOffersNftOffer
+                  key={offer.subOfferKey.toBase58()}
+                  offerAmount={offer.offerAmount}
+                  APR={offer.aprNumerator}
+                  status={offer.state}
+                  offerID={offer.subOfferKey}
+                  duration={offer.loanDuration}
+                  repaid={offer.minRepaidNumerator}
+                />
+              ))
+            ) : (
+              <div className={`my-offers-no__offers ${classNames ? classNames : ''}`}>
+                <p>You have no offers yet</p>
+              </div>
+            )}
+          </div>
+        </div>
+        {console.log(showLightboxLoan)}
+        {showLightboxLoan !== null ? (
+          <Lightbox classNames='create-loan-lightbox'>
+            <CreateLoan onClick={() => {}} />
+          </Lightbox>
+        ) : null}
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
