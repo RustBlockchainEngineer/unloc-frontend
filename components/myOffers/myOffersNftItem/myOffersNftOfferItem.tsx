f31ddcc0c3a25c2404da09b473995a15c37a2f73
Octoturge <iwo.strzebonski@gmail.com>
2022-04-26 14:01:48 +0200

    done

--- components/myOffers/myOffersNftItem/myOffersNftOfferItem.tsx
+++ components/myOffers/myOffersNftItem/myOffersNftOfferItem.tsx
@@ -47,7 +47,7 @@ export const MyOffersNftOfferItem: React.FC<MyOffersNftOfferItemProps> = ({
 
       <div className='offer__row'>
         <div className='offer__row--item'>
-          <h4>Offer ID</h4>
+          <h4>Collateral ID</h4>
           <div className='suboffer-containers__id'>{compressAddress(4, offerID.toString())}</div>
         </div>
         <div className='offer__row--item'>
