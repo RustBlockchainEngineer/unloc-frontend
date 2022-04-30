2539e9b3a25008281a1fee74d06097a9f85ed456
Octoturge <iwo.strzebonski@gmail.com>
2022-04-30 17:39:49 +0200

    Added user authentication system

--- components/storeDataAdapter.tsx
+++ components/storeDataAdapter.tsx
@@ -4,6 +4,8 @@ import { useWallet } from '@solana/wallet-adapter-react'
 import { StoreContext } from '@pages/_app'
 import { initLoanProgram } from '@integration/nftLoan'
 import { PublicKey } from '@solana/web3.js'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 type Props = {
   children?: React.ReactNode
@@ -14,14 +16,42 @@ export const StoreDataAdapter: React.FC<Props> = observer(({ children }: Props) 
   const store = useContext(StoreContext)
 
   useEffect(() => {
-    if (connected && wallet) {
-      initLoanProgram(wallet.adapter)
-      store.GlobalState.fetchGlobalState()
-      store.Wallet.setConnected(connected)
-      store.Wallet.setWallet(wallet)
-      store.Wallet.setHandleDisconnect(disconnect)
-      store.Wallet.setWalletKey(publicKey as PublicKey)
+    disconnect()
+  }, [])
+
+  useEffect(() => {
+    const setWallet = async () => {
+      if (connected && wallet && publicKey) {
+        const response = await axios.post('/api/auth', { user: publicKey.toBase58() })
+
+        if (!(response && response.data)) return
+
+        const { isWhitelisted } = response.data
+
+        if (!isWhitelisted) {
+          toast.error('You are not whitelisted!', {
+            autoClose: 3000,
+            position: 'top-center',
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined
+          })
+          disconnect()
+          return
+        }
+
+        initLoanProgram(wallet.adapter)
+        store.GlobalState.fetchGlobalState()
+        store.Wallet.setConnected(connected)
+        store.Wallet.setWallet(wallet)
+        store.Wallet.setHandleDisconnect(disconnect)
+        store.Wallet.setWalletKey(publicKey as PublicKey)
+      }
     }
+
+    setWallet()
   }, [wallet, connected, store.Wallet])
 
   return <>{children}</>
---
+++ pages/api/auth.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { isUserWhitelisted } from '@integration/db'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<{ isWhitelisted: boolean }>) => {
+  if (req.method === 'POST') {
+    const isWhitelisted = await isUserWhitelisted(req.body.user)
+    res.status(200).json({ isWhitelisted })
+  } else {
+    res.end(404)
+  }
+}
+
+export default handler
