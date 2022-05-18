b95e705fdc51e3a8538270a823c83b664f0479d0
Hoxton <igorkalicinski@gmail.com>
2022-05-18 17:16:58 +0200

    changed active li.a look

--- components/layout/topMenu.tsx
+++ components/layout/topMenu.tsx
@@ -14,17 +14,17 @@ export const TopMenu = () => {
     <ul className="top-menu">
       <li>
         <Link href="/">
-          <a className={handleCurrent("/")}>{localesTop.home}</a>
+          <a className={handleCurrent("/")}><div className={handleCurrent("/") + ' mobile-wrapper'}>{localesTop.home}</div></a>
         </Link>
       </li>
       <li>
         <Link href="/my-offers">
-          <a className={handleCurrent("/my-offers")}>{localesTop.myOffers}</a>
+          <a className={handleCurrent("/my-offers")}><div className={handleCurrent("/my-offers") + ' mobile-wrapper'}>{localesTop.myOffers}</div></a>
         </Link>
       </li>
       <li>
         <Link href="/loans-given">
-          <a className={handleCurrent("/loans-given")}>{localesTop.loansGiven}</a>
+          <a className={handleCurrent("/loans-given")}><div className={handleCurrent("/loans-given") + ' mobile-wrapper'}>{localesTop.loansGiven}</div></a>
         </Link>
       </li>
     </ul>
