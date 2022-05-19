352b62d33ba23a836032630c9719ece132aa1755
iziuk <izyuk.web@gmail.com>
2022-05-19 18:46:42 +0300

    hot-fix: DEPOSIT NFT functionality

--- pages/api/nfts/whitelisted.ts
+++ pages/api/nfts/whitelisted.ts
@@ -2,12 +2,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getWhitelisted } from "@integration/db";
 
-export const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse<string[]>,
-): Promise<void> => {
+const handler = async (req: NextApiRequest, res: NextApiResponse<string[]>): Promise<void> => {
   if (req.method === "POST") {
     const data = await getWhitelisted();
     res.status(200).json(data);
   } else res.end(404);
 };
+
+export default handler;
