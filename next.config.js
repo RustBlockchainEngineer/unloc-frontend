5f0d6904cf622ad3a8c3b6ab608b60b6a23fcb40
Dmytro Iziuk <Izyuk.web@gmail.com>
2022-05-19 15:35:05 +0300

    Eslint setup (#118)
    * feat: setup eslint & lunched app
    
    * feat: setup eslint & lunched app
     - filters fixes
    
    * feat: setup eslint & lunched app
     - filters fixes
     - added .eslintignore to split eslint fixes in several steps.
    
    * fix: from @types importing to "offers" module
    
    * fix:
    - rename "offers" to "nft" module
    
    * fix:
    - fixed folder naming for better structure
    
    * fix:
    - fixed errors
    
    * Update filter.tsx
    
    Fixed TS7030: Not all code paths return a value. on line 224
    
    * Update customMultiSelect.tsx
    
    Removed unused value
    
    * Update userToolbox.tsx
    
    Removed unused useState
    
    * fix:
    - fixed deployment errors
    
    * fix:
    - fixed deployment errors
    
    * fix:
    - fixed deployment errors
    
    * fix:
    - fixed deployment errors
    
    * fix:
    - fixed deployment errors
    
    * Merge branch 'eslint_setup' of https://github.com/UNLOC-DEV/unloc into eslint_setup
    
    * fix: fixed package.json errors
    
    * fix:
    - fixed package.json errors
    
    * fix:
    - fixed package.json errors
    
    * fix:
    - fixed package.json errors
    
    * test:
    - testing husky
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged

--- next.config.js
+++ next.config.js
@@ -12,9 +12,8 @@ const nextConfig = {
   },
   webpack5: true,
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
+    if (!isServer) config.resolve.fallback.fs = false;
+
     return config;
   },
 };
