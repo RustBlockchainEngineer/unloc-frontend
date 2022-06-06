29a033b059019610074d56ada8d6e797cbe23d14
praskoson <40672437+praskoson@users.noreply.github.com>
2022-06-06 11:10:31 +0200

    temp fix for build error

--- components/profile/stakeAccount/durationProgress.tsx
+++ components/profile/stakeAccount/durationProgress.tsx
@@ -1,5 +1,5 @@
 import { CircularProgressBar } from "@components/layout/circularProgressBar";
-import dayjs from "dayjs";
+// import dayjs from "dayjs";
 
 interface IDurationProgress {
   startUnix: number;
@@ -8,10 +8,14 @@ interface IDurationProgress {
 }
 
 export const DurationProgress = ({ startUnix, endUnix, label = "days" }: IDurationProgress) => {
-  const startDate = dayjs.unix(startUnix);
-  const endDate = dayjs.unix(endUnix);
-  const normalizedMax = endUnix - startUnix;
-  const normalizedStart = dayjs().unix() - startUnix;
+  // const startDate = dayjs.unix(startUnix);
+  // const endDate = dayjs.unix(endUnix);
+  // const normalizedMax = endUnix - startUnix;
+  // const normalizedStart = dayjs().unix() - startUnix;
+  console.log(startUnix);
+  console.log(endUnix);
+
+  // Maybe move the markup that shows the dates around the circle here
 
   return <CircularProgressBar value={30} maxValue={90} label={label} />;
 };
