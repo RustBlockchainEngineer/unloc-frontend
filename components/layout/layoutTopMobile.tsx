1214e6220c611737e726ec976db0f7ac8caaf820
izyuk <izyuk.web@gmail.com>
2022-06-14 16:25:18 +0300

    close menu on scroll

--- components/layout/layoutTopMobile.tsx
+++ components/layout/layoutTopMobile.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { TopMenu } from "./topMenu";
 import { UserToolbox } from "./userToolbox";
 
 export const LayoutTopMobile = () => {
   const [menuVisible, setMenuVisibility] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  });
+
+  const scrollHandler = useCallback(() => {
+    setMenuVisibility(false);
+  }, []);
 
   return (
     <>
