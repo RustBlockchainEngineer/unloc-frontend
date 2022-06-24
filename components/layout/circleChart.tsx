348af420b65af46bc225bd0654beba8ea335ff3e
Hoxton <igorkalicinski@gmail.com>
2022-06-24 19:19:41 +0200

    finalizing design

---
+++ components/layout/circleChart.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CircleChart = () => {
+  return <div className="circle-chart">Chart WIP</div>;
+};
--- components/voting/votingPage.tsx
+++ components/voting/votingPage.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { CircleChart } from "@components/layout/circleChart";
+import { PieChart } from "react-minimal-pie-chart";
 
 export const VotingPage = () => {
   const [votingPower, setVotingPower] = React.useState(400);
+  console.log("PIE", PieChart.defaultProps.radius);
+
+  const nextVoteValues = { SMB: 55, DAA: 20, DeGods: 10, DT: 10, SolGods: 5 };
+
+  const colorList = [
+    "#d63abe",
+    "#0262d2",
+    "#02d2d2",
+    "#1fd202",
+    "#d2ca02",
+    "#9c02d2",
+    "#d2025a",
+    "#d20202",
+    "#d27f02",
+    "#b5d202",
+    "#02d26e",
+    "#c202d2",
+    "#258f00",
+    "#930069",
+    "#005593",
+    "#00a711",
+    "#930000",
+    "#00936f",
+    "#000f93",
+    "#a24e00",
+  ];
+
+  const voteData = [
+    { title: "SMB", value: 55, color: colorList[0] },
+    { title: "DAA", value: 20, color: colorList[1] },
+    { title: "DeGods", value: 10, color: colorList[2] },
+    { title: "DT", value: 10, color: colorList[3] },
+    { title: "SolGods", value: 5, color: colorList[4] },
+  ];
+
+  const nextVoteData = voteData.map((vote) => {
+    return { title: vote.title, value: nextVoteValues[vote.title], color: vote.color };
+  });
 
   return (
     <div className="voting-page">
@@ -10,8 +50,51 @@ export const VotingPage = () => {
       </div>
       <div className="voting-page__distribution">
         <div className="voting-page__distribution--wrapper">
-          <div className="this-week"></div>
-          <div className="next-week"></div>
+          <div className="this-week">
+            <div className="this-week__header">THIS WEEK’S DISTRIBUTION</div>
+
+            <div className="this-week__percent-column">
+              {voteData.map(function (data, index) {
+                return (
+                  <div className="row" key={data.title}>
+                    <span className="row__title">
+                      <div
+                        className={`row__title--dot color--${index + 1}`}
+                        style={{ backgroundColor: data.color }}
+                      />
+                      {data.title}:
+                    </span>
+                    <span className="row__data">{data.value}%</span>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="this-week__chart">
+              <PieChart data={[...voteData]} animate={true} lineWidth={25} />
+            </div>
+          </div>
+          <div className="next-week">
+            <div className="this-week__header">THIS WEEK’S DISTRIBUTION</div>
+
+            <div className="this-week__percent-column">
+              {nextVoteData.map(function (data, index) {
+                return (
+                  <div className="row" key={data.title}>
+                    <span className="row__title">
+                      <div className={`row__title--dot color--${index + 1}`} />
+                      {data.title}:
+                    </span>
+                    <span className="row__data">{data.value}%</span>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="this-week__chart">
+              <PieChart data={[...nextVoteData]} animate={true} lineWidth={25} />
+            </div>
+          </div>
         </div>
 
         <div className="voting-page__distribution__button">
