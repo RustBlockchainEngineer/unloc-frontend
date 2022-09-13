50c48bb42418ab53bc8c4a90dc6f500254c680e6
praskoson <40672437+praskoson@users.noreply.github.com>
2022-09-13 16:20:00 +0200

    Update the devnet USDC token
    Matches the contract

--- constants/currency-constants.ts
+++ constants/currency-constants.ts
@@ -3,7 +3,7 @@ export const SOL = "SOL";
 
 export type CurrencyTypes = "USDC" | "SOL";
 
-export const USDC_MINT_DEVNET = "ExW7Yek3vsRJcapsdRKcxF9XRRS8zigLZ8nqqdqnWgQi";
+export const USDC_MINT_DEVNET = "GH1gUyAw7ems5MD46WGC9JPMHncLVBkHagpXgtYVUyPr";
 export const USDC_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 export const SOL_MINT_DEVNET = "So11111111111111111111111111111111111111112";
 export const SOL_MINT = "So11111111111111111111111111111111111111112";
